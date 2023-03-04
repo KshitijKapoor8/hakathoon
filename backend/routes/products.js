393d842cf196566c3459bc66529fa17feb648e07
KshitijKapoor8 <kshitijkapoor8@gmail.com>
2023-03-04 15:52:35 -0600

    asfasf

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -53,6 +53,7 @@ router.route('/add').post((req, res) => {
     ]);
     
     page = newPage;
+    let score = 0;
 
     searchLink = page.url();
       // Wait for the JavaScript on the page to finish executing
@@ -104,32 +105,83 @@ router.route('/add').post((req, res) => {
     
     
       await browser.close();
+
+      function similarity(s1, s2) {
+        var longer = s1;
+        var shorter = s2;
+        if (s1.length < s2.length) {
+          longer = s2;
+          shorter = s1;
+        }
+        var longerLength = longer.length;
+        if (longerLength == 0) {
+          return 1.0;
+        }
+        return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+      }
+
+      function editDistance(s1, s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+      
+        var costs = new Array();
+        for (var i = 0; i <= s1.length; i++) {
+          var lastValue = i;
+          for (var j = 0; j <= s2.length; j++) {
+            if (i == 0)
+              costs[j] = j;
+            else {
+              if (j > 0) {
+                var newValue = costs[j - 1];
+                if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                  newValue = Math.min(Math.min(newValue, lastValue),
+                    costs[j]) + 1;
+                costs[j - 1] = lastValue;
+                lastValue = newValue;
+              }
+            }
+          }
+          if (i > 0)
+            costs[s2.length] = lastValue;
+        }
+        return costs[s2.length];
+      }
       
 
-      // Write the CSV string to a file using the fs module
-      console.log("AHHHHHHHHH " + ingredients)
       fs.readFile('output.json', 'utf8', (err, data) => {
           if (err) throw err;
       
           // Parse the JSON data into a JavaScript object
           const obj = JSON.parse(data);
+          var l = Object.keys(obj).length;
 
+          
+
+          let c = 0;
+          let d = 0;
           for (let i = 0; i < ingredients.length; i++) {
-            if (obj[ingredients[i]] != null) {
-                console.log("FOUND");
+            for (let j = 0; j < l; j++) {
+              if(similarity(ingredients[i], Object.keys(obj)[j]) > 0.9) {
+                c++;
+                d += Object.values(obj)[j];
+              }
             }
           }
-      
+          
+          let score = d / c;
+          score = Math.round(score);
+          console.log(score);
+
+
+          
+          const newPr = new Pr({name, ingredients, score, link});
+          newPr.save()
+              .then(() => {res.json(newPr)})
+              .catch((err) => {res.status(400).json("Error: "+ err)})
           //console.log(obj);
       });
               
-
-
       
-      const newPr = new Pr({name, ingredients, score, link});
-      newPr.save()
-          .then(() => {res.json(newPr)})
-          .catch((err) => {res.status(400).json("Error: "+ err)})
     })(); 
       
 
